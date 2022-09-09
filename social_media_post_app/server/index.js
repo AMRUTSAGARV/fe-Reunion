dccd54e6a364b9aee3fcaa3ccf94830e851b0638
Amrut Sagar <amrutsagar06@gmail.com>
2022-09-09 19:10:14 +0530

    getting started with social media mern app

---
+++ social_media_post_app/server/index.js
@@ -0,0 +1,9 @@
+// express framework for creating the routing of our application.
+import express from "express";
+// body-parser  -> enable us to send post request
+//momet -> date
+//react-file-base -> convert the images
+//edux-thunk used for asynchronous actions using redux.
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
