e2ae5ae9658cc79e2d93f0aeae366177f3935bdc
Amrut Sagar <amrutsagar06@gmail.com>
2022-09-12 00:08:28 +0530

    styling

--- social_media_post_app/client/src/App.js
+++ social_media_post_app/client/src/App.js
@@ -1,9 +1,40 @@
 import React from "react";
+import film from "./images/film.png";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from "./styles";
+
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>hhhd</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Film Palette
+        </Typography>
+        <img className={classes.image} src={film} alt="film" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            {/* xs 12 means that its going to take full width on extra small devices and on small and medium its gonna be seven which means that its going to take seven out of twelve spaces on small or large devices     */}
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
